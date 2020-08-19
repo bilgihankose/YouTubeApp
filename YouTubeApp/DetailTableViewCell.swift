c9a2a654080c1b61ce24b13a966e6efbabd93322
Bilgihan Kose <bilgihankose@gmail.com>
2020-08-19 09:28:31 +0300

    add dummy json & table cell

---
+++ YouTubeApp/DetailTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  DetailTableViewCell.swift
+//  YouTubeApp
+//
+//  Created by Bilgihan Köse on 19.08.2020.
+//
+
+import UIKit
+
+class DetailTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ YouTubeApp/Video.swift
@@ -0,0 +1,8 @@
+//
+//  Video.swift
+//  YouTubeApp
+//
+//  Created by Bilgihan Köse on 19.08.2020.
+//
+
+import Foundation
